5697b2d59a32b3f14e597f70673c5dc07d4afc4c
Harsh <harsh.bhalodiya@kevit.io>
2025-08-08 11:21:48 +0530

    Added mongodb connection code

--- backend/outreachhub-backend-nest/src/app.module.ts
+++ backend/outreachhub-backend-nest/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('mongo_db'),
+      }),
+    }),
+    AdminModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
